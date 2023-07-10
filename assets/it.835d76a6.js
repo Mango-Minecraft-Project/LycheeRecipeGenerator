bd2ac7dc3abaaba4c7658ebf980014c36a3e3b2d
Snownee <i@snownee.com>
2023-07-10 19:18:48 +0800

    .

---
+++ assets/it.835d76a6.js
@@ -0,0 +1,2 @@
+const i=JSON.parse(`{"advancement":"Avanzamento","copy":"Copia","dimension":"Dimensione","dimension_type":"Tipo di Dimensione","download":"Scarica","fields":"Campi","item_modifier":"Modificatore d'Oggetti","language":"Lingua","loot_table":"Tavola del Bottino","predicate":"Predica","redo":"Rifai"}`);export{i as default};
+//# sourceMappingURL=it.835d76a6.js.map
